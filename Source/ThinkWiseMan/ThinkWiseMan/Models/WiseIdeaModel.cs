0b2278b65db50f1904a20df7a828f09159d49198
Igor Tishchenko <tuugarin@hotmail.com>
2015-11-20 00:30:17 +0300

    Added XmlDataService and WiseIdea Model

---
+++ Source/ThinkWiseMan/ThinkWiseMan/Models/WiseIdeaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkWiseMan.Models
+{
+    public class WiseIdeaModel
+    {
+            public string Author { get; set; }
+            public int Day { get; set; }
+            public int Month { get; set; }
+            public string Content { get; set; }
+    }
+}
